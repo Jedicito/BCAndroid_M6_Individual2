2965b8aaa53b9f24ed48dcd72c0906e1746bc7fb
Jedicito <seron.cesar@gmail.com>
2023-07-28 21:19:36 -0400

    Hito 1

---
+++ app/src/main/java/chl/ancud/m6_individual2/Repository.kt
@@ -0,0 +1,16 @@
+package chl.ancud.m6_individual2
+
+import androidx.lifecycle.LiveData
+import chl.ancud.m6_individual2.data.local.Item
+import chl.ancud.m6_individual2.data.local.ItemDao
+
+class Repository(private val itemDao: ItemDao) {
+
+    suspend fun insertItem(item: Item) {
+        itemDao.insertItem(item)
+    }
+
+    fun getItemes(): LiveData<List<Item>> {
+        return itemDao.getItemes()
+    }
+}
